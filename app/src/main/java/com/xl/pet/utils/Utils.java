ee8408bdf420c15caf3cd053bf3e06abac27fb60
xinlin.huang <xinlin.huang@gitlab.bt.com>
2023-05-11 18:37:42 +0800

    升级gradle，jdk版本

--- app/src/main/java/com/xl/pet/utils/Utils.java
+++ app/src/main/java/com/xl/pet/utils/Utils.java
@@ -7,43 +7,36 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.util.Log;
 import android.util.TypedValue;
 
 import androidx.annotation.RequiresApi;
 
 import java.util.List;
 
-import static com.xl.pet.constants.Constants.LOG_TAG;
 
 public class Utils {
     // 判断是否在桌面
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
     public static boolean isHome(Context context) {
         String topActivity = "";
-        //android5.0以上
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            UsageStatsManager m = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
-            if (null!=m){
-                long now = System.currentTimeMillis();
-                //obtain recent in 1 hour app status
-                List<UsageStats> stats = m.queryUsageStats(UsageStatsManager.INTERVAL_BEST, now - 60 * 1000 * 60, now);
-                //obtain recent run app，maybe running app
-                if ((stats != null) && (!stats.isEmpty())) {
-                    int j = 0;
-                    for (int i = 0; i < stats.size(); i++) {
-                        if (stats.get(i).getLastTimeUsed() > stats.get(j).getLastTimeUsed()) {
-                            j = i;
-                        }
+        //api 22以上
+        UsageStatsManager m = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        if (null!=m){
+            long now = System.currentTimeMillis();
+            List<UsageStats> stats = m.queryUsageStats(UsageStatsManager.INTERVAL_BEST, now - 60 * 1000 * 60, now);
+            if ((stats != null) && (!stats.isEmpty())) {
+                int j = 0;
+                for (int i = 0; i < stats.size(); i++) {
+                    if (stats.get(i).getLastTimeUsed() > stats.get(j).getLastTimeUsed()) {
+                        j = i;
                     }
-                    topActivity = stats.get(j).getPackageName();
                 }
-                Log.d(LOG_TAG,"顶部activity"+topActivity);
+                topActivity = stats.get(j).getPackageName();
             }
-            return "com.android.launcher3".equals(topActivity)
-                    || "com.google.android.apps.nexuslauncher".equals(topActivity);
+//                Log.d(LOG_TAG,"顶部activity"+topActivity);
         }
-        return true;
+        return "com.android.launcher3".equals(topActivity)
+                || "com.google.android.apps.nexuslauncher".equals(topActivity);
     }
 
 
